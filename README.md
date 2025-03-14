4d836bcb5acb14b279afa5f96d3119fb289bc10c
MiguelHenri <miguelbrahen@gmail.com>
2025-03-14 15:09:43 -0300

    integrate postgreSQL

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Cypher
-A password manager API built with ASP.NET Core
+
+A password manager API built with ASP.NET Core and PostgreSQL.
+
+## Dependencies
+
+Make sure you have .NET SDK installed. This repo uses version 9.0.201.
+
+## Run
+
+To run the API, execute the following command inside the `/backend` directory:
+
+```bash
+dotnet run
+```
---
+++ backend/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DotNetEnv;
+using backend.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+var connectionString = Env.GetString("DB_CONN");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
